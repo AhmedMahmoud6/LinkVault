d07156b872b47f79a1c8e6d27c868f917a318852
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2025-05-24 16:09:11 +0300

    add link validation & tasks rendering

--- functions.js
+++ functions.js
@@ -1,22 +1,100 @@
+let idCounter = 0;
+
 function validateForm(taskVal, urlVal, addBookmark) {
   let existingError = document.querySelector(".err");
+
+  // regex for real links
+  const isValidUrl = /^https?:\/\/[a-zA-Z0-9.-]+\.[a-z]{2,}/.test(urlVal);
+
+  // if taskVal and urlVal are not empty
   if (taskVal && urlVal) {
+    // if the link input is valid
+    if (!isValidUrl) {
+      createErrMsg(existingError, "Link is Inappropriate");
+      return false;
+    }
+
+    if (existingError) existingError.remove();
+
     addTask.classList.remove("ring-4", "ring-[#ff858b]");
     addUrl.classList.remove("ring-4", "ring-[#ff858b]");
 
-    if (existingError) existingError.remove();
     return true;
   }
-  if (document.querySelector(".err")) return;
 
-  let validationMsg = document.createElement("p");
-  validationMsg.textContent = "Both fields are required!";
-  validationMsg.classList = "err text-[#ff858b] font-bold";
+  if (existingError) return false;
 
-  addBookmark.before(validationMsg);
+  createErrMsg(existingError, "Both fields are required!");
 
   if (!taskVal) addTask.classList.add("ring-4", "ring-[#ff858b]");
 
   if (!urlVal) addUrl.classList.add("ring-4", "ring-[#ff858b]");
   return false;
+}
+
+function createBookmark(taskVal, urlVal, bookMarksList) {
+  let bookmark = {
+    taskVal,
+    urlVal,
+  };
+
+  bookMarksList.unshift(bookmark);
+}
+
+function renderTasks(bookMarksList, emptyState, bookmarksParent) {
+  if (bookMarksList.length < 1) {
+    emptyState.classList.remove("hidden");
+    bookmarksParent.classList.add("hidden");
+  } else {
+    emptyState.classList.add("hidden");
+    bookmarksParent.classList.remove("hidden");
+
+    idCounter += 1;
+    bookmarksParent.innerHTML = "";
+
+    for (let i of bookMarksList) {
+      let bookmarkHtml = `
+        <div
+            class="task bg-[#243044] border-t-4 border-[#f97316] h-50 p-4 flex flex-col justify-between rounded outline-2 outline-[#333f52] cursor-pointer shadow-md hover:-translate-y-2 transition-all"
+            id = "${idCounter}"
+          >
+            <div class="top-side">
+              <h1 class="title text-white font-bold">
+                ${i.taskVal}
+              </h1>
+              <a
+                href="${i.urlVal}"
+                class="url text-[#f97316] hover:underline truncate w-80 max-[550px]:w-full block"
+                target="_blank"
+                >${i.urlVal}</a
+              >
+            </div>
+            <div class="bottom-side flex gap-4">
+              <button
+                type="button"
+                class="edit-btn text-white w-20 bg-[#fbbf24] cursor-pointer hover:bg-[#f8d066] focus:ring-3 focus:ring-[#d49901] font-medium rounded-lg text-sm px-5 py-2.5"
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                class="delete-btn text-white w-20 bg-[#ef4444] cursor-pointer hover:bg-[#ef6868] focus:ring-3 focus:ring-[#be0707] font-medium rounded-lg text-sm px-5 py-2.5"
+              >
+                Delete
+              </button>
+            </div>
+        </div>
+        `;
+      bookmarksParent.insertAdjacentHTML("beforeend", bookmarkHtml);
+    }
+  }
+}
+
+function createErrMsg(existingError, errMsg) {
+  if (existingError) existingError.remove();
+  let validationMsg = document.createElement("p");
+  validationMsg.textContent = errMsg;
+  validationMsg.classList = "err text-[#ff858b] font-bold";
+
+  addBookmark.before(validationMsg);
 }
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let addTask = document.querySelector(".add-title");
 let addUrl = document.querySelector(".add-url");
 let addBookmark = document.querySelector(".add-btn");
+let emptyState = document.querySelector(".empty-state");
+let bookmarksParent = document.querySelector(".bookmarks");
 
 let bookMarksList = [];
+renderTasks(bookMarksList, emptyState, bookmarksParent);
 
 addBookmark.addEventListener("click", (_) => {
   let taskVal = addTask.value.trim();
@@ -11,11 +14,7 @@ addBookmark.addEventListener("click", (_) => {
     addTask.value = "";
     addUrl.value = "";
 
-    let bookmark = {
-      taskVal,
-      urlVal,
-    };
-
-    bookMarksList.unshift(bookmark);
+    createBookmark(taskVal, urlVal, bookMarksList);
+    renderTasks(bookMarksList, emptyState, bookmarksParent);
   }
 });
